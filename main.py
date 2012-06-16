ae62ba1285eec7e4ad19078a3f91877e31356793
mizu-tama <tamaki.mizuno@gmail.com>
2012-06-16 15:38:22 +0900

    タイプミス修正。メインページの表示に成功

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def  get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.repsponse.out.write('Follow @polka__bot')
+        self.response.out.write('Follow @polka__bot')
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
