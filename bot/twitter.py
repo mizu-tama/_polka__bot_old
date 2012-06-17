c439ca24d47608463202510ed4bb00d6eb37aba8
mizu-tama <tamaki.mizuno@gmail.com>
2012-06-17 17:27:33 +0900

    メソッドの命名規則をアンスコにする

--- bot/twitter.py
+++ bot/twitter.py
@@ -3,12 +3,12 @@ import tweepy
 import config as cnf
 
 class TwitterAuth(object):
-    def getApi(self):
+    def get_api(self):
         auth = tweepy.OAuthHandler(cnf.consumer_key, cnf.consumer_secret)
         auth.set_access_token(cnf.access_key, cnf.access_secret)
         api =  tweepy.API(auth_handler=auth)
         return api
 
     def post(self, tweet):
-        api = self.getApi()
+        api = self.get_api()
         api.update_status(tweet)
